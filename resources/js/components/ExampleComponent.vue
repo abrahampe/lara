98ac73c3161709205ddfe94b5cfe1e393c976580
Abraão Pessoa <abraaopessoa@yahoo.com.br>
2020-06-03 01:16:46 -0300

    First commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        //php phpDocumentor.phar -d . -t docs/api
+        mounted() {
+            console.log('Component started.')
+            axios.get(
+    'http://www.mocky.io/v2/5ed7144932000035002744d0'
+).then((response) => {
+       console.log('Component new axios.')
+    console.log(response);
+    return Promise.resolve();
+});
+        }
+    }
+</script>
