2a444ea862ba39e7652754926a938d9c02e6a7a5
MrAbraham <abraaopessoa@yahoo.com.br>
2020-06-19 14:23:14 -0300

    Search Added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,11 @@
                                 </li>
                             @endif
                         @else
+                        <form class="form-inline my-2 my-lg-0">
+                  <input class="form-control mr-sm-2" type="text" placeholder="Search" />
+                  <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                </form>
+
                             <li class="nav-item dropdown">
                             <a id="navbarDropdown" href="#" class="py-0 nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 
